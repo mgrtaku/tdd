2081f9181bc31ec6fbc23ea0629f92a24064f611
mgrtaku <megurotkm24@gmail.com>
2024-06-17 14:17:14 +0900

    test:閉区間[3,8]を与えたときに下端点3を返すテスト

---
+++ exercise1/src/main/java/com/example/ClosedRange.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class ClosedRange {
+    int i;
+    int ii;
+
+    public ClosedRange(int i, int i1) {
+        this.i = i;
+        this.ii = ii;
+    }
+
+    public int getLowerEndpoint() {
+        return 0;
+    }
+}
---
+++ exercise1/src/test/java/com/example/ClosedRangeTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClosedRangeTest {
+    @Test
+    void 閉区間3_8を与えたときに下端点3を返す(){
+        //準備
+        ClosedRange range = new ClosedRange(3, 8);
+        //実行
+        //検証
+        int actual = range.getLowerEndpoint();
+        assertEquals(3, actual);
+    }
+}
