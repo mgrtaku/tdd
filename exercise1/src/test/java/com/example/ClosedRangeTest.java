2d69f917a42f0a33ad5ce8f7ae1df55aff837e6b
YusukeMaedaTrain <yusuke.maeda.0423.sb@gmail.com>
2024-06-17 14:39:35 +0900

    refactor:2箇所重複していた箇所をBeforeEachを利用してリファクタリング

--- exercise1/src/test/java/com/example/ClosedRangeTest.java
+++ exercise1/src/test/java/com/example/ClosedRangeTest.java
@@ -1,25 +1,26 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClosedRangeTest {
+
+    private ClosedRange range;
+
+    @BeforeEach
+    void setUp() {
+        range = new ClosedRange(3, 8);
+    }
+
     @Test
     void 閉区間3_8を与えたときに下端点3を返す(){
-        //準備
-        ClosedRange range = new ClosedRange(3, 8);
-        //実行
-        //検証
         assertEquals(3, range.getLowerEndpoint());
     }
 
     @Test
     void 閉区間3_8を与えたときに上端点8を返す(){
-        //準備
-        ClosedRange range = new ClosedRange(3, 8);
-        //実行
-        //検証
         assertEquals(8, range.getUpperEndpoint());
     }
 }
